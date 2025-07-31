1c21d6f9af460cb29535fdf321f865112fd1281c
Pravat Behera <beherapravat301@gmail.com>
2025-07-31 19:59:31 +0530

    feat: add App and Button components

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Button({
+    title,
+    id,
+    rightIcon,
+    leftIcon,
+    containerClassName = 'bg-blue-100',
+
+
+}) {
+    return (
+        <button
+            id={id}
+            className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClassName}`}
+            leftIcon={leftIcon}
+            rightIcon={rightIcon}
+
+        >{title}</button>
+    )
+}
+
+export default Button
